6672d733f579249d0513e53bd42d919c78391fd2
shubha-tekgro <shubha@gmail.com>
2026-01-08 11:56:59 +0545

    files@

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./assets/**/*.css"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
